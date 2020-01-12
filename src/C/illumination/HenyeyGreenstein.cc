3fa31f79f141677fc2b20d80118901bc515c5353
Raphael Scheible <mail@raphiniert.com>
2020-01-12 16:38:46 +0100

    code cleanup

--- src/C/illumination/HenyeyGreenstein.cc
+++ src/C/illumination/HenyeyGreenstein.cc
@@ -1,4 +1,4 @@
-/*! \file volumeRender.h
+/*! \file HenyeyGreenstein.cc
  * 	\author Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
  * 	\version 1.0
  *  \license This project is released under the GNU Affero General Public License, Version 3
@@ -13,98 +13,8 @@
 #include <sstream>
 #include <stdio.h>
 #include <string.h>
+#include <float3.h>
 
-/*! \struct float3
- * 	\brief struct of a 3D float
- */
-struct float3 {
-  /*! The x component */
-  float x;
-  /*! The y component */
-  float y;
-  /*! The z component */
-  float z;
-};
-
-/*! \fn float3 make_float3(float x, float y, float z)
- * 	\brief constructing a float3 structure [x,y,z]
- *  \param x the x component
- *  \param x the y component
- *  \param x the z component
- *  \return structure of type float3
- */
-inline float3 make_float3(float x, float y, float z) {
-  float3 result;
-  result.x = x;
-  result.y = y;
-  result.z = z;
-
-  return result;
-}
-
-/*! \fn float dot(const float3& a,const float3& b)
- * 	\brief computes the dot-product of two vectors
- *  \param a first vector
- *  \param b second vector
- *  \return dot product (scalar)
- */
-inline float dot(const float3 &a, const float3 &b) {
-  return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-/*! \fn float3 make_float3(float * aPointer)
- * 	\brief constructing a float3 structure [x,y,z]
- *  \param aPointer pointer the first element
- *  \return structure of type float3
- */
-inline float3 make_float3(float *aPointer) {
-  return make_float3(aPointer[0], aPointer[1], aPointer[2]);
-}
-
-/*! \struct float3x3
- * 	\brief struct of a 3x3 matrix of floats
- */
-struct float3x3 {
-  /*! The cols of the matrix */
-  float3 col[3];
-};
-
-/*! \fn float3 operator*( const float3x3& m, const float3& v )
- * 	\brief multiplication operand of matrix times vector
- *  \param m a 3x3 matrix
- *  \param v a vector
- *  \return the resulting vector
- */
-float3 operator*(const float3x3 &m, const float3 &v) {
-  float3 result;
-
-  result.x = m.col[0].x * v.x + m.col[1].x * v.y + m.col[2].x * v.z;
-  result.y = m.col[0].y * v.x + m.col[1].y * v.y + m.col[2].y * v.z;
-  result.z = m.col[0].z * v.x + m.col[1].z * v.y + m.col[2].z * v.z;
-
-  return result;
-}
-
-/*! float3x3 rotateAroundX( float angle )
- * 	\brief rotatation around the x-component [1,0,0]
- *  \param angle rotation angle
- *  \return rotation matrix
- */
-float3x3 rotateAroundX(float angle) {
-  float s = sinf(angle);
-  float c = cosf(angle);
-  float3x3 m;
-  m.col[0].x = 1;
-  m.col[1].x = 0;
-  m.col[2].x = 0;
-  m.col[0].y = 0;
-  m.col[1].y = c;
-  m.col[2].y = s;
-  m.col[0].z = 0;
-  m.col[1].z = -s;
-  m.col[2].z = c;
-  return m;
-}
 
 #define MIN_ARGS 1
 #define PI ((float)3.141592653589793238462643383279502884197169399375105820)
@@ -126,7 +36,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   if (nrhs < MIN_ARGS)
     mexErrMsgTxt("insufficient parameter!");
 
-  float aG(0.8);
+  float aG = 0.8;
 
   if (nrhs == 2)
     aG = (float)mxGetScalar(prhs[1]);
@@ -139,16 +49,16 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   mwSize dim[3] = {volumeSize, volumeSize, volumeSize};
   mxArray *resultArray = mxCreateNumericArray(3, dim, mxSINGLE_CLASS, mxREAL);
 
-  float frac_full(PI2 / volumeSize);
-  float frac_half(PI / volumeSize);
+  float frac_full = PI2 / volumeSize;
+  float frac_half = PI / volumeSize;
 
   float *outData = (float *)mxGetPr(resultArray);
 
-  float alpha(0), beta(0), gamma(0);
+  float alpha = 0, beta = 0, gamma = 0;
   float3 lightIn, lightOut;
   float3x3 rotMatrix;
 
-  int pageSize(volumeSize * volumeSize);
+  int pageSize = volumeSize * volumeSize;
 
   for (int c = 0; c < volumeSize; ++c) {
     gamma = c * frac_full;
