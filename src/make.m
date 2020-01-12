4e6b88e922a968c3e52de5516955532ff1425a1c
Raphael Scheible <mail@raphiniert.com>
2020-01-12 20:23:07 +0100

    comment

--- src/make.m
+++ src/make.m
@@ -1,3 +1,5 @@
+% compiling required mex files
+
 % build render command
 mexcuda C/render.cpp C/volumeRender.cpp C/volumeRender_kernel.cu -I"../lib/cuda-samples/Common/" -I"C/" -lcuda -output matlab/volumeRender
 
