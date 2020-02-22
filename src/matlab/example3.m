e0f6b20d518f3e0785ac8665891b18dbc91a071e
Raphael Scheible <mail@raphiniert.com>
2020-02-22 14:10:53 +0100

    refactored example

--- src/matlab/example3.m
+++ src/matlab/example3.m
@@ -7,10 +7,6 @@ addpath('VolumeRender');
 % some tests for the matlab-C++ connection
 path='/home/scheiblr/git/raphiniert/volumerenderer/data/';
 
-% l = [LightSource([1,2,3],[3,3,4]), LightSource([0,1,9],[13,4,6])];
-
-% filename = [path 'tmp_st37_Emb2.h5']; dataset = '/t0/channel3';
-
 filename = [path '/ViBE-Z_72hpf_v1.h5']; 
 
 dataset = '/anatomy/average_brain';
@@ -19,102 +15,167 @@ elementSizeUm = h5readatt(filename, dataset,'element_size_um');
 
 dataset = '/expression/3A10';
 data_structure = h5read(filename, dataset);
-emission_structure = Volume(data_structure);
 
 % create volumes
 emission_structure = Volume(data_structure);
 emission_main = Volume(data_main);
 
+%% setup for rendering main channel
+
+% total frames of the movie
+total_frames=120;
+
+% rotation performed in the entire movie
+rotation_of_movie=1200;
+
+
+% create empty structure for all rendered images
+rendered_images_main = zeros([  size(emission_structure.Data,2), ...
+                                size(emission_structure.Data,1), ...
+                                3, ...
+                                total_frames]);
+
+
 % create render object
 render = VolumeRender();
 
-% central_z = round(size(emission.Data,3)/2);
-% imshow(emission.Data(:,:,central_z)' * 10,[]);
+% setup render options
+render.Color = [1,1,1];
 
-% setup illumination settings
-render.VolumeIllumination=Volume(HenyeyGreenstein(64));
-render.LightSources = LightSource([-15,15,0], [1,1,1]);
+render.ScaleEmission=1;
+render.ScaleAbsorption=2;
+render.ScaleReflection=1;
+
 render.ElementSizeUm=elementSizeUm;
 render.FocalLength=3.0;
 render.DistanceToObject = 6;
-render.rotate(-90,0,0);
 render.OpacityThreshold=0.95;
 
-% rotate some more
+% rotate
+render.rotate(-90,0,0);
 render.rotate(0,45,0);
 
-% % setup image size (of the resulting 2D image)
-% render.ImageResolution= size(emission_structure.Data,[1 2]);
-% 
-% % set render volumes
-% render.VolumeEmission=emission_structure;
-% render.VolumeAbsorption=emission_structure;
-% render.ScaleReflection=1;
-% 
-% render.Color = [1,1,0];
-% 
-% rendered_image_1 = render.render();
-% 
-% data_absorption = data_main;
-% half_size=size(data_absorption)/2;
-% data_absorption(1:end, 1:half_size(2), 1:end) = 0;
-% absorptionVolume=Volume(data_absorption);
-% absorptionVolume.resize(half_size);
-% absorptionVolume.normalize(0,1);
-% 
-% render.VolumeEmission=emission_main;
-% render.VolumeAbsorption=absorptionVolume;
-% render.ScaleReflection=0.5;
-% render.Color = [1,1,1];
-% 
-% rendered_image_2 = render.render();
-% 
-% imshow(rendered_image_1+rendered_image_2);
-% return;
+% setup illumintation
+render.LightSources = LightSource([-15,15,0], [1,1,1]);
 
+% setup volumes
+data_absorption = data_main;
+absorptionVolume=Volume(data_absorption);
+half_size=size(data_absorption)/2;
+absorptionVolume.resize(half_size);
+absorptionVolume.normalize(0,1);
+
+render.VolumeAbsorption=absorptionVolume;
+render.VolumeEmission=emission_main;
+
+render.VolumeIllumination=Volume(HenyeyGreenstein(64));
 
 % setup image size (of the resulting 2D image)
-render.ImageResolution=size(emission_structure.Data,[1 2]);
+render.ImageResolution=size(emission_main.Data,[1 2]);
+
+% rotation per step
+beta = rotation_of_movie/total_frames;
 
 
-nStep=24;
-rendered_image = zeros([size(emission_structure.Data,2),size(emission_structure.Data,1), 3, 360/nStep]);
+%% start renderung main channel
+disp('rendering main channel');
 
-beta = 360/nStep;
-for i=1:nStep
-    display(strcat('image ', num2str(i)));
 
-    % set render volumes
-    render.VolumeEmission=emission_structure;
-    render.VolumeAbsorption=emission_structure;
-    render.ScaleReflection=1;
+start_frame=1;
+end_frame=total_frames/4;
+for i=start_frame:end_frame
+    disp(strcat(' -image ', num2str(i)));
 
-    render.Color = [1,1,0];
+    rendered_image = render.render();
+    
+    % rotate object
+    render.rotate(0,beta,0);
+    
+    rendered_images_main(:,:,:,i) = rendered_image;
+end
 
-    rendered_image_1 = render.render();
+start_frame=(total_frames/4)+1;
+end_frame=total_frames/2;
+for i=start_frame:end_frame
+    disp(strcat(' -image ', num2str(i)));
 
-    data_absorption = data_main;
-    half_size=size(data_absorption)/2;
-    data_absorption(1:end, 1:half_size(2), 1:end) = 0;
     absorptionVolume=Volume(data_absorption);
+    half_size=size(data_absorption)/2;
+    data_absorption(1:end, 1:half_size(2), 1:end) = (i/start_frame)/(end_frame);
     absorptionVolume.resize(half_size);
     absorptionVolume.normalize(0,1);
 
-    render.VolumeEmission=emission_main;
     render.VolumeAbsorption=absorptionVolume;
-    render.ScaleReflection=0.5;
-    render.Color = [1,1,1];
 
-    rendered_image_2 = render.render();
+
+    rendered_image = render.render();
     
     % rotate object
     render.rotate(0,beta,0);
 
-    result = rendered_image_1+rendered_image_2;
-    rendered_image(:,:,:,i) = result/max(result(:));
+    rendered_images_main(:,:,:,i) = rendered_image;
 end
 
-normalizedImages = VolumeRender.normalizeSequence(rendered_image);
+start_frame=(total_frames/2)+1;
+end_frame=total_frames;
+for i=start_frame:end_frame
+    disp(strcat(' -image ', num2str(i)));
+    
+    rendered_image = render.render();
+    
+    % rotate object
+    render.rotate(0,beta,0);
 
-mov = immovie(normalizedImages);
+    rendered_images_main(:,:,:,i) = rendered_image;
+end
+
+
+%% render structure image channel
+% create empty structure for all rendered images
+rendered_images_structure = zeros([  size(emission_structure.Data,2), ...
+                                size(emission_structure.Data,1), ...
+                                3, ...
+                                total_frames]);
+                            
+disp('rendering structure channel');
+
+% reset render
+render.RotationMatrix = eye(3);
+render.rotate(-90,0,0);
+render.rotate(0,45,0);
+
+% setup render
+render.ImageResolution=size(emission_structure.Data,[1 2]);
+
+
+render.VolumeAbsorption=emission_structure;
+render.VolumeEmission=emission_structure;
+
+render.ScaleEmission=1;
+render.ScaleAbsorption=0.6;
+render.ScaleReflection=1;
+
+render.Color = [1,1,0];
+
+
+start_frame=1;
+end_frame=total_frames;
+for i=start_frame:end_frame
+    disp(strcat(' -image ', num2str(i)));
+
+    rendered_image = render.render();
+    
+    % rotate object
+    render.rotate(0,beta,0);
+    
+    rendered_images_structure(:,:,:,i) = rendered_image;
+end
+
+
+%% create one movie
+rendered_images_combined = rendered_images_main+rendered_images_structure;
+
+normalized_images = VolumeRender.normalizeSequence(rendered_images_combined);
+
+mov = immovie(normalized_images);
 implay(mov);
