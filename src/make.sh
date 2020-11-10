67ee8f00acc9ad0e3ff8033ef93abd4d53554300
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2020-11-10 09:32:08 +0100

    moved memory manager

--- src/make.sh
+++ src/make.sh
@@ -5,4 +5,4 @@ mex_c="/home/scheiblr/Spielewiese/R2020b/bin/mex"
 $mex_c -largeArrayDims -f /home/scheiblr/Spielewiese/R2020b/toolbox/parallel/gpu/extern/src/mex/glnxa64/nvcc_g++.xml NVCC_FLAGS="" -v -cxx -D_DEBUG -largeArrayDims C/mex/render.cpp C/vr/volumeRender.cpp C/vr/volumeRender_kernel.cu -I"../lib/cuda-samples/Common/" -I"C/common" -I"C/vr/" -lcuda -output matlab/VolumeRender/volumeRender 
 $mex_c -cxx -largeArrayDims C/mex/timestamp.cpp -output matlab/VolumeRender/timestamp
 $mex_c -largeArrayDims C/mex/HenyeyGreenstein.cc -I"C/vr/illumination" -output matlab/VolumeRender/HenyeyGreenstein
-$mex_c -largeArrayDims C/mex/mmanager.cpp -I"C/common" -output C/memory_manager/mmanager
+$mex_c -largeArrayDims C/mex/mmanager.cpp -I"C/common" -output matlab/memory_manager/mmanager
